df607eb4514c1a631e956d403d182a48ef7df93b
TiagoDesc <nobretiago96@gmail.com>
2024-08-27 15:30:18 -0300

    Adding a new registration method

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -1,5 +1,7 @@
 package med.voll.api.controller;
 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -7,5 +9,10 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("medicos")
 
 public class MedicoController {
+
+    @PostMapping
+    public void cadastrar(@RequestBody String json) {
+        System.out.println(json);
+    }
 
 }
