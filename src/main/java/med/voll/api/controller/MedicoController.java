822cc8c741bbd62c526ecf12e79c1ffbe5a48c72
TiagoDesc <nobretiago96@gmail.com>
2024-08-27 15:52:34 -0300

    New class

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -4,6 +4,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import med.voll.api.medico.DadosCadastroMedico;
 
 @RestController
 @RequestMapping("medicos")
@@ -11,8 +12,8 @@ import org.springframework.web.bind.annotation.RestController;
 public class MedicoController {
 
     @PostMapping
-    public void cadastrar(@RequestBody String json) {
-        System.out.println(json);
+    public void cadastrar(@RequestBody DadosCadastroMedico dados) {
+        System.out.println(dados);
     }
 
 }
