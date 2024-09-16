eb824b1660cd2db5b255cd992fc5fff3e9fc5539
TiagoDesc <nobretiago96@gmail.com>
2024-09-16 11:31:20 -0300

    new import

--- src/main/java/med/voll/api/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/medico/DadosCadastroMedico.java
@@ -1,8 +1,7 @@
 package med.voll.api.medico;
 
-import org.hibernate.validator.constraints.Email;
-
 import jakarta.validation.Valid;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Pattern;
@@ -10,11 +9,11 @@ import med.voll.api.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
 
-                @NotBlank String nome,
-                @NotBlank @Email String email,
-                @NotBlank String telefone,
-                @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
-                @NotNull Especialidade especialidade,
+        @NotBlank String nome,
+        @NotBlank @Email String email,
+        @NotBlank String telefone,
+        @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
+        @NotNull Especialidade especialidade,
 
-                @NotNull @Valid DadosEndereco endereco) {
+        @NotNull @Valid DadosEndereco endereco) {
 }
