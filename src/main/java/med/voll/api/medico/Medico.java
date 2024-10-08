e7f4afb26c758dee3362d1473545032cc04203fe
TiagoDesc <nobretiago96@gmail.com>
2024-10-08 20:24:42 -0300

    Add new method delete and new atribute

--- src/main/java/med/voll/api/medico/Medico.java
+++ src/main/java/med/voll/api/medico/Medico.java
@@ -37,7 +37,10 @@ public class Medico {
     @Embedded
     private Endereco endereco;
 
+    private Boolean ativo;
+
     public Medico(DadosCadastroMedico dados) {
+        this.ativo = true;
         this.nome = dados.nome();
         this.email = dados.email();
         this.crm = dados.crm();
@@ -48,15 +51,19 @@ public class Medico {
     }
 
     public void atualizarInformacoes(@Valid DadosAtualizacaoMedico dados) {
-        if(dados.nome() != null){
+        if (dados.nome() != null) {
             this.nome = dados.nome();
         }
-        if(dados.telefone() != null){
+        if (dados.telefone() != null) {
             this.telefone = dados.telefone();
         }
-        if(dados.endereco() != null){
+        if (dados.endereco() != null) {
             this.endereco.atualizarInformacoes(dados.endereco());
         }
-        
+
+    }
+
+    public void excluir() {
+        this.ativo = false;
     }
 }
