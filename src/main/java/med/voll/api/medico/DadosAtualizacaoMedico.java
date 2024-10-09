93125500b9773fdc5ea797f685d16f9e5109be82
TiagoDesc <nobretiago96@gmail.com>
2024-10-08 22:37:18 -0300

    notNull

--- src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
+++ src/main/java/med/voll/api/medico/DadosAtualizacaoMedico.java
@@ -1,13 +1,12 @@
 package med.voll.api.medico;
 
-import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import med.voll.api.endereco.DadosEndereco;
 
 public record DadosAtualizacaoMedico(
-    @NotBlank
-    Long id, 
-    String nome, 
-    String telefone, 
-    DadosEndereco endereco) {
-    
+        @NotNull Long id,
+        String nome,
+        String telefone,
+        DadosEndereco endereco) {
+
 }
