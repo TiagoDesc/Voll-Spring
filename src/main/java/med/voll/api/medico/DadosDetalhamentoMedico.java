7f38c37a66e1f4946bdb145d6be761330da9fc63
TiagoDesc <nobretiago96@gmail.com>
2024-11-09 20:15:41 -0300

    insert name

--- src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
+++ src/main/java/med/voll/api/medico/DadosDetalhamentoMedico.java
@@ -2,11 +2,13 @@ package med.voll.api.medico;
 
 import med.voll.api.endereco.Endereco;
 
-public record DadosDetalhamentoMedico(Long id, String email, String crm, String telefone, Especialidade especialidade,
+public record DadosDetalhamentoMedico(Long id, String nome, String email, String crm, String telefone,
+        Especialidade especialidade,
         Endereco endereco) {
 
     public DadosDetalhamentoMedico(Medico medico) {
-        this(medico.getId(), medico.getEmail(), medico.getCrm(), medico.getTelefone(), medico.getEspecialidade(),
+        this(medico.getId(), medico.getNome(), medico.getEmail(), medico.getCrm(), medico.getTelefone(),
+                medico.getEspecialidade(),
                 medico.getEndereco());
 
     }
