ea16f83a54620343348345271d25ab57bdfd1ca3
TiagoDesc <nobretiago96@gmail.com>
2024-11-09 20:36:46 -0300

    new method

--- src/main/java/med/voll/api/controller/MedicoController.java
+++ src/main/java/med/voll/api/controller/MedicoController.java
@@ -69,4 +69,11 @@ public class MedicoController {
 
     }
 
+    @GetMapping("/{id}")
+    public ResponseEntity detalhar(@PathVariable Long id) {
+        var medico = repository.getReferenceById(id);
+        return ResponseEntity.ok(new DadosDetalhamentoMedico(medico));
+
+    }
+
 }
