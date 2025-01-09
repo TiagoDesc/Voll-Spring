19cf2330ed78ac00fd241eb7c03ffc38039ce5ed
TiagoDesc <nobretiago96@gmail.com>
2025-01-08 21:08:03 -0300

    updating in class

--- src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
+++ src/main/java/med/voll/api/paciente/DadosDetalhamentoPaciente.java
@@ -1,5 +1,12 @@
 package med.voll.api.paciente;
 
-public class DadosDetalhamentoPaciente {
+import med.voll.api.endereco.Endereco;
+
+public record DadosDetalhamentoPaciente(String nome, String email, String telefone, String cpf, Endereco endereco) {
+
+    public DadosDetalhamentoPaciente(Paciente paciente) {
+        this(paciente.getNome(), paciente.getEmail(), paciente.getTelefone(), paciente.getCpf(),
+                paciente.getEndereco());
+    }
 
 }
