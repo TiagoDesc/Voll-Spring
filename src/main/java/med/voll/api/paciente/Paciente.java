3330807105c21089d53dac911d7b2d5aeac02e51
TiagoDesc <nobretiago96@gmail.com>
2025-01-08 21:38:15 -0300

    Changing the class name

--- src/main/java/med/voll/api/paciente/Paciente.java
+++ src/main/java/med/voll/api/paciente/Paciente.java
@@ -46,10 +46,10 @@ public class Paciente {
     public void atualizarInformacoes(DadosAtualizacaoPaciente dados) {
         if (dados.nome() != null)
             this.nome = dados.nome();
-    
+
         if (dados.telefone() != null)
             this.telefone = dados.telefone();
-    
+
         if (dados.endereco() != null)
             endereco.atualizarInformacoes(dados.endereco());
     }
