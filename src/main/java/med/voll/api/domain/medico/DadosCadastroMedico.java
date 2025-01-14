25483100ab08c4784498df868a22e27d5e7d9d5e
TiagoDesc <nobretiago96@gmail.com>
2025-01-13 22:01:21 -0300

    Messages in bean validation

--- src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DadosCadastroMedico.java
@@ -9,11 +9,10 @@ import med.voll.api.domain.endereco.DadosEndereco;
 
 public record DadosCadastroMedico(
 
-                @NotBlank String nome,
-                @NotBlank @Email String email,
-                @NotBlank String telefone,
-                @NotBlank @Pattern(regexp = "\\d{4,6}") String crm,
-                @NotNull Especialidade especialidade,
-
-                @NotNull @Valid DadosEndereco endereco) {
+        @NotBlank(message = "Nome é obrigatório") String nome,
+        @NotBlank(message = "Formato do email é inválido") @Email String email,
+        @NotBlank(message = "Telefone é obrigatório") String telefone,
+        @NotBlank(message = "CRM é obrigatório") @Pattern(regexp = "\\d{4,6}", message = "Formato do CRM é inválido") String crm,
+        @NotNull @NotNull(message = "Especialidade é obrigatória") Especialidade especialidade,
+        @NotNull @NotNull(message = "Dados do endereço são obrigatórios") @Valid DadosEndereco endereco) {
 }
